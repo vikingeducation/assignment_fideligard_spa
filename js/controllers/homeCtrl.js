e071a5da599f76ba6106b8e215aa9f95ddb21910
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-09-17 14:46:07 -0400

    ajax working

--- js/controllers/homeCtrl.js
+++ js/controllers/homeCtrl.js
@@ -1,30 +1,77 @@
-fideligard.controller("homeCtrl",['$scope', function($scope){
-$scope.dateDiff = 0;
+fideligard.controller("homeCtrl",['$scope', '$http', function($scope, $http){
 
-today = new Date()
-d = today
-$watch $scope.selectedDate = d.setDate(today.getDate()- $scope.dateDiff);
+  //DQRQ
+  var symbols = ['FB', 'TWTR', 'LNKD', 'GOOG']
+  oneYearAgo = function(){
+    d = new Date()
+    $scope.selectedDate = d.setDate(d.getDate() - (365));
+    return new Date($scope.selectedDate)
+  }
+  $scope.selectDate = function(){
+    d = new Date()
+    $scope.selectedDate = d.setDate(d.getDate() - (-$scope.dateDiff));
+    sendDate = new Date($scope.selectedDate)
+    $scope.dateOutput = $scope.getDateStr(sendDate)
+    getData()
+  };
+  $scope.getDateStr = function(date){
+    var dd = date.getDate();
+    var mm = date.getMonth()+1; //January is 0!
+    var yyyy = date.getFullYear();
 
-getDateStr = function(date){
-  var dd = date.getDate();
-  var mm = date.getMonth()+1; //January is 0!
-  var yyyy = date.getFullYear();
+    if(dd<10) {
+        dd='0'+dd
+    }
 
-  if(dd<10) {
-      dd='0'+dd
+    if(mm<10) {
+        mm='0'+mm
+    }
+    return dateStr = yyyy+'-'+mm+'-'+dd;
+
   }
 
-  if(mm<10) {
-      mm='0'+mm
+  var results = []
+  var ajaxSuccess = function(response){
+    results.push(response.data.query.results.quote)
+    getData();
   }
-  return dateStr = mm+'/'+dd+'/'+yyyy;
+  var ajaxFailure = function(){}
+  var buildUrl = function(symbol){
+    return "http://query.yahooapis.com/v1/public/yql?q= \
+        select * from   yahoo.finance.historicaldata \
+                 where  symbol    = '"+ symbol + "' \
+                 and    startDate = '"+ $scope.getDateStr(oneYearAgo()) + "' \
+                 and    endDate   = '"+ $scope.getDateStr(new Date()) + "' \
+        &format=json \
+        &diagnostics=true \
+        &env=store://datatables.org/alltableswithkeys \
+        &callback="
+  };
 
-}
+  (function(){
+    for (var i = 0; i < symbols.length; i++){
+      var sym = symbols[i];
+      var url = buildUrl(sym);
+      console.log(url)
+      $http.get(url).then(ajaxSuccess, ajaxFailure)
+    }
 
+  })()
 
+  var getData = function(){
+    var data = []
+    for (var i = 0; i < results.length; i++){
+      data.push($.grep(results[i], function(val){
+        return val.Date == $scope.dateOutput
+      }))
+    }
+    $scope.stockData = data
+  }
 
-
-
+  $scope.dateDiff = -1;
+  d = new Date()
+  $scope.selectedDate = new Date(d.setDate(d.getDate() - 1));
+  $scope.dateOutput = $scope.getDateStr($scope.selectedDate);
 
 
 }])
