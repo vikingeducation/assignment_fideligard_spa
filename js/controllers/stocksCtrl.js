6172a4d2d8fde44145540f78f9c3dc803d2ba18f
Alice Fung <alice.o.fung@gmail.com>
2015-09-18 01:11:08 -0400

    odd changes to fix filter

--- js/controllers/stocksCtrl.js
+++ js/controllers/stocksCtrl.js
@@ -9,12 +9,38 @@ stocks.controller('stocksCtrl',
     $scope.history = stocksService.getAllStockData();
     $scope.dateObj = stocksService.dateObj;
 
+    $scope.lastSort = {};
 
-
-    $scope.priceChangeNDays = function(input, stock){
-      console.log('inside controller price change');
-      console.log('date '+ $scope.dateSelected);
-      return stocksService.priceChangeNDays(input, stock);
+    $scope.sortSelection = function(input){
+      console.log('sorting');
+      // debugger;
+      if (!input) return "";
+      if (input != $scope.lastSort.type){
+        $scope.lastSort = {type: input, order: '+'};
+        return $scope.sortSelect = "+" + input;
+      }
+      else if ($scope.lastSort.order == '+'){
+        $scope.lastSort.order = '-';
+        return $scope.sortSelect = "-" + input;
+      }
+      else {
+        $scope.lastSort.order = '-';
+        return $scope.sortSelect = "+" + input;
+      }
     };
 
+    $scope.getInfo = function(){
+      debugger
+      var list = [];
+      for (key in $scope.history) {
+        var arr = list[key];
+        var dateInfo = $filter('filter')(arr, {date: $scope.dateObj.currentdate});
+        list.push(dateInfo);
+        debugger
+      }
+      return list;
+    };
+    var list = $scope.getInfo()
+
+    $scope.list = list;
   }]);
