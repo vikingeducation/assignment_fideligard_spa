af3ab84034b101ea166e35001d7da081a65ef4c7
alokp <alokpra@gmail.com>
2015-09-19 00:23:25 -0700

    epic 1 complete

--- javascript/controllers/stocksCtrl.js
+++ javascript/controllers/stocksCtrl.js
@@ -6,13 +6,69 @@ tradeApp.controller('stocksCtrl',  [  '$scope',
 
 // allStocks = stockDataService.getAllStockData();
 // stock = stockDataService.getStocksTableData();
-$scope.stocks = stockDataService.getStocksTableData();
 
-$scope.symbols = stockDataService.getSymbols();
+  $scope.nameSort     = 0;
+  $scope.priceSort    = 0;
+  $scope.onedaySort   = 0;
+  $scope.sevendaySort = 0;
+  $scope.monthSort    = 0;
 
-$scope.symbolReturn = function(symbolFilter){
-  var i = $scope.symbols.indexOf(symbolFilter);
-  return $scope.symbols[i];
-};
+  $scope.stocks = stockDataService.getStocksTableData();
+  $scope.symbols = stockDataService.getSymbols();
+
+  $scope.symbolReturn = function(symbolFilter){
+    var i = $scope.symbols.indexOf(symbolFilter);
+    return $scope.symbols[i];
+  };
+
+  $scope.label = function(prop){
+    var attr = {  'Symbol':'nameSort', 'Price':'priceSort', '1d':'onedaySort',
+                  '7d':'sevendaySort', '30d':'monthSort'  };
+    return [prop, prop+' \u25BC', prop+' \u25B2'][$scope[attr[prop]] % 3];
+  };
+
+  $scope.sortCriteria = function(){
+    var nameArr     = [null, "-name", "+name"];
+    var priceArr    = [null, "-price", "+price"];
+    var oneDayArr   = [null, "-oneday", "+oneday"];
+    var sevenDayArr = [null, "-sevenday", "+sevenday"];
+    var monthArr    = [null, "-month", "+month"];
+
+    var nameCriteria      = nameArr[$scope.nameSort % 3];
+    var priceCriteria     = priceArr[$scope.priceSort % 3];
+    var oneDayCriteria    = oneDayArr[$scope.onedaySort % 3];
+    var sevenDayCriteria  = sevenDayArr[$scope.sevendaySort % 3];
+    var monthCriteria     = monthArr[$scope.monthSort % 3];
+
+    var criteria = [];
+
+    if (nameCriteria) criteria.push(nameCriteria);
+    if (priceCriteria) criteria.push(priceCriteria);
+    if (oneDayCriteria) criteria.push(oneDayCriteria);
+    if (sevenDayCriteria) criteria.push(sevenDayCriteria);
+    if (monthCriteria) criteria.push(monthCriteria);
+
+    return (criteria);
+  };
+
+  $scope.cycleName = function(){
+    $scope.nameSort++;
+  };
+
+  $scope.cyclePrice = function(){
+    $scope.priceSort++;
+  };
+
+  $scope.cycleOneDay = function(){
+    $scope.onedaySort++;
+  };
+
+  $scope.cycleSevenDay = function(){
+    $scope.sevendaySort++;
+  };
+
+  $scope.cycleMonth = function(){
+    $scope.monthSort++;
+  };
 
 }]);
