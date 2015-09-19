8f91a0abf00ccd09847ed295dd195df47e507a6c
facingsouth <xin.d.liu@gmail.com>
2015-09-18 18:38:20 -0700

    add css make table fit div

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module("app", ['ui.router']);
+var app = angular.module("app", ['ui.router', "ui.bootstrap"]);
 
 app.config(function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise('/index/portfolio');
