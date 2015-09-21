5af4f13afc90f78f7fd5fd3df844fdb873b80c38
alokp <alokpra@gmail.com>
2015-09-20 21:56:51 -0700

    update trade form

--- javascript/controllers/tradeCtrl.js
+++ javascript/controllers/tradeCtrl.js
@@ -30,39 +30,31 @@ $scope.$watchGroup(['symbol', 'date'], function(newValues, oldValues){
   });
 
 var updateFormInfo = function(newValues, oldValues){
-  var symbol  = newValues[0].toUpperCase();
-  var date    = newValues[1];
-  // var symbolFound = false;
-  // var dateFound = false;
 
-  // Check symbol match
-  var companyData = stockDataService.getAllStockData()[symbol];
+  if (newValues[0] !== undefined){
+    var symbol  = newValues[0].toUpperCase();
+    var date    = newValues[1];
+    // var symbolFound = false;
+    // var dateFound = false;
 
-  // Check date match
-  if (companyData){
-    // symbolFound = true;
-  for(var i = 0; i < companyData.length; i++){
-      if(companyData[i]['Date'] === date){
-        $scope.symbol = companyData[i].Symbol;
-        $scope.price = companyData[i].Open;
-        $scope.date = date;
-        // dateFound = true;
+    // Check symbol match
+    var companyData = stockDataService.getAllStockData()[symbol];
+
+
+    // Check date match
+    if (companyData){
+      // symbolFound = true;
+    for(var i = 0; i < companyData.length; i++){
+        if(companyData[i]['Date'] === date){
+          $scope.symbol = companyData[i].Symbol;
+          $scope.price = companyData[i].Open;
+          $scope.date = date;
+          // dateFound = true;
+        }
       }
     }
+
   }
-
-  // if (!dateFound){
-  //   $scope.dateFound = 'Date out of range';
-  // } else {
-  //   $scope.dateFound = null;
-  // }
-
-  // if (!symbolFound){
-  //   $scope.symbolFound = 'Symbol not found';
-  // } else {
-  //   $scope.dateFound = null;
-  // }
-
 };
 
 
