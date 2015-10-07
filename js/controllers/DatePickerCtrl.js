237e9dcea041b9c8124d97bc6d5b50d9706b1db6
Adam Kinson <adam.kinson@gmail.com>
2015-10-07 17:21:11 -0400

    minor refactor in date controller

--- js/controllers/DatePickerCtrl.js
+++ js/controllers/DatePickerCtrl.js
@@ -7,21 +7,26 @@ fideligard.controller('DatePickerCtrl',
     $scope.maxDate = Number(new Date('12/31/2014'));
     $scope.step = 1000*60*60*24;
     $scope.currentDate = $scope.minDate;
-    $scope.currentDateText = $filter('date')($scope.currentDate, "M/d/yyyy")
     $scope.rangePercent = 0;
 
 
-    $scope.updateLabel = function() {
+    ($scope.setCurrentDateText = function() {
       $scope.currentDateText = $filter('date')($scope.currentDate, "M/d/yyyy")
-      var label = angular.element(document.querySelector('.datepicker-label'));
+    })();
+
+
+    $scope.updateLabel = function() {
+      $scope.setCurrentDateText();
       $scope.rangePercent = ($scope.currentDate - $scope.minDate)/($scope.maxDate - $scope.minDate);
+      var label = angular.element(document.querySelector('.datepicker-label'));
       label.css('margin-left', $scope.rangePercent*100 + '%');
     }
 
 
     $scope.unfocus = function() {
       document.dateTextForm.dateText.blur();
-    }
+    };
+
 
     $scope.updateDateText = function() {
       var input = new Date(document.dateTextForm.dateText.value);
@@ -31,9 +36,8 @@ fideligard.controller('DatePickerCtrl',
         $scope.updateLabel();
       } else {
         console.log('value out of range');
-        $scope.currentDateText = $filter('date')($scope.currentDate, "M/d/yyyy");
+        $scope.setCurrentDateText();
       };
-
-    }
+    };
 
   }]);
