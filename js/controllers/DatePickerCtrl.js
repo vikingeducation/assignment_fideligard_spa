2a21b6e1f215ca86598a22244370c60e950a8620
Adam Kinson <adam.kinson@gmail.com>
2015-10-07 17:10:43 -0400

    add date updating via text

--- js/controllers/DatePickerCtrl.js
+++ js/controllers/DatePickerCtrl.js
@@ -9,7 +9,7 @@ fideligard.controller('DatePickerCtrl',
     $scope.currentDate = $scope.minDate;
     $scope.currentDateText = $filter('date')($scope.currentDate, "M/d/yyyy")
     $scope.rangePercent = 0;
-    $scope.dateInput = false;
+
 
     $scope.updateLabel = function() {
       $scope.currentDateText = $filter('date')($scope.currentDate, "M/d/yyyy")
@@ -18,18 +18,20 @@ fideligard.controller('DatePickerCtrl',
       label.css('margin-left', $scope.rangePercent*100 + '%');
     }
 
-    $scope.inputDate = function() {
-      // create text input
-      // focus that input
-      $scope.dateInput = true;
-      var inputField = angular.element(document.querySelector('#datepicker-input'));
-      inputField.css('left', $scope.rangePercent*100 + '%').css('top', 0)
-      /*
-      var label = angular.element(document.querySelector('.datepicker-label'));
-      var newInput = angular.element("<input type=text id='datepicker-input' ng-model='currentDate'>")
-      label.append(newInput);
-      newInput.css('left', $scope.labelX).css('top', $scope.labelY);
-      */
+
+    $scope.unfocus = function() {
+      document.dateTextForm.dateText.blur();
+    }
+
+    $scope.updateDateText = function() {
+      var input = document.dateTextForm.dateText.value;
+      // validate
+
+      // update $scope.currentDate
+      console.log($scope.currentDate);
+      $scope.currentDate = Number(new Date(input));
+      // update position?
+      console.log($scope.currentDate);
     }
 
   }]);
