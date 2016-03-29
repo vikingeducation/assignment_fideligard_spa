3f234f1dda2a9a1c0bed984471b83dabdd726ddc
Thomas Lo <thomasjinlo@gmail.com>
2016-03-29 10:50:53 -0700

    make api calls

---
+++ js/services/stockService.js
@@ -0,0 +1,12 @@
+fideligard.factory('stockService',
+  ['$http',
+    function($http) {
+
+      var _stocks = [];
+      var _symbols = ['GOOG', 'AMBA', 'ABM', 'AAPL', 'PSEC', 'RAVE', 'ODFL', 'YHOO', 'IEZ', 'NE'];
+
+      var query = function() {
+        stock = {};
+        $http.get('http://query.yahooapis.com/v1/public/yql?q=%20select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20=%20%22GOOG%22%20and%20startDate%20=%20%222015-01-01%22%20and%20endDate%20=%20%222015-12-31%22%20&format=json%20&diagnostics=true%20&env=store://datatables.org/alltableswithkeys%20&callback=');
+      };
+}]);
