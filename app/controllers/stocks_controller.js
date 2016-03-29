188eb3f7d1a7e8c52f313ec1d0903dc284ec0957
Kit Langton <kitlangton@gmail.com>
2016-03-29 19:49:00 -0400

    whee

--- app/controllers/stocks_controller.js
+++ app/controllers/stocks_controller.js
@@ -1,64 +1,31 @@
 stocks.controller('StocksCtrl', ['$http', '$scope', '$stateParams', 'PortfolioService', function($http, $scope, $stateParams, PortfolioService) {
 
-  function date_by_subtracting_days(date, days) {
-    return new Date(
-        date.getFullYear(),
-        date.getMonth(),
-        date.getDate() + days,
-        date.getHours(),
-        date.getMinutes(),
-        date.getSeconds(),
-        date.getMilliseconds()
-        );
-  }
 
+  $scope.symbolFilter = ''
   $scope.currentSymbols =  PortfolioService.getSymbols();
-  console.log( $scope.currentSymbols );
 
   $scope.currentDate = new Date();
   $scope.stocks = {};
   $scope.currentSymbols.forEach( function(sym){
     $scope.stocks[sym] = {};
-    var symbolFileString = '/' + sym + '_data.json'
+    var symbolFileString = '/' + sym + '_data.json';
     $http.get(symbolFileString).then(function(response) {
       response.data.dataset.data.forEach(function(daily) {
         $scope.stocks[sym][daily[0]] = daily[4];
+        $scope.stocks[sym].symbol = sym;
       });
     });
   });
-
-
-    $http.get('/GE_data.json').then(function(response) {
-        console.log('test here');
-        console.log(response);
-    });
-
-    $http.get('/AAPL_data.json').then(function(response) {
-        console.log('test here');
-        console.log(response);
-    });
-
-  //
-  // $scope.stocks.ORCL = {};
-  console.log($scope.stocks);
-
-
-  $scope.stockDifference = function(values, daysAgo) {
-    return $scope.closeFor(values,0) - $scope.closeFor(values,daysAgo);
-  };
-
-  $scope.closeFor = function(values, daysAgo) {
-    if (!daysAgo) {
-      daysAgo = 0;
-    }
-    var value;
-    var count = 0;
-    while (!value && count > -10) {
-      var date = date_by_subtracting_days($scope.currentDate, count - daysAgo);
-      var isoDate = date.toISOString().slice(0, 10);
-      value = values[isoDate];
-      count -= 1;
-    }
-    return value;
-  };
 }]);
+
+stocks.filter('stockFilter', function() {
+    return function( items, search ) {
+      var returnedItems = {};
+      for (var key in items) {
+        if (key.substring(0, search.length) == search) {
+          returnedItems[key] = items[key];
+        }
+      }
+      return returnedItems
+    };
+});
--- app/directives/stock_display.js
+++ app/directives/stock_display.js
@@ -1,17 +1,57 @@
-
 stocks.directive('stockDisplay', [ function() {
-
   console.log('stock display directive');
+
+  function date_by_subtracting_days(date, days) {
+    return new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        date.getDate() + days,
+        date.getHours(),
+        date.getMinutes(),
+        date.getSeconds(),
+        date.getMilliseconds()
+        );
+  }
+
+  var stockDifference = function(values, daysAgo, currentDate) {
+    return closeFor(values,0,currentDate) - closeFor(values,daysAgo, currentDate);
+  };
+
+  var closeFor = function(values, daysAgo, currentDate) {
+    if (!daysAgo) {
+      daysAgo = 0;
+    }
+    var value;
+    var count = 0;
+    while (!value && count > -10) {
+      var date = date_by_subtracting_days(currentDate, count - daysAgo);
+      var isoDate = date.toISOString().slice(0, 10);
+      value = values[isoDate];
+      count -= 1;
+    }
+    return value;
+  };
+
+  link = function(scope) {
+    scope.$watch('currentDate', function() {
+      scope.closer = closeFor(scope.values,0,scope.currentDate);
+      scope.oneDay = stockDifference(scope.values,1,scope.currentDate);
+      scope.sevenDays = stockDifference(scope.values,7,scope.currentDate);
+      scope.thirtyDays = stockDifference(scope.values,30,scope.currentDate);
+    });
+  };
 
   var def = {
     restrict: 'AE',
     scope: {
-      // values: '@',
-      // dayDiff: '@',
-      difference: '@'
+      stock: '=',
+      values: '=',
+      currentDate: '=',
     },
-    // scope: true,
+    link: link,
     templateUrl: 'app/directives/stock_display.html',
   };
+
   return def;
+
 }]);
