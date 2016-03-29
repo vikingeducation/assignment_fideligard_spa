cd2e9c71ff85037d7a183cd78c9790323509369a
Joshua Masland-Sarani <j.masland@gmail.com>
2016-03-29 18:08:23 -0400

    service working

--- js/controllers/stocks_controller.js
+++ js/controllers/stocks_controller.js
@@ -1,26 +1,5 @@
 financialApp.controller('StocksCtrl', ['$scope', 'dateService', function($scope, dateService){
 
-  console.log("hello");
-
-  $scope.startDate = dateService.getStartDate();
-  $scope.endDate = dateService.getEndDate();
-  $scope.dateSelection = 0;
-
-  $scope.convertDate = function(date){
-    dateService.convertDate(date);
-  }
-
-  $scope.currentDate = function() {
-    var start = $scope.startDate.getTime();
-    var selection = $scope.dateSelection * (1000*60*60*24);
-    var newDate = new Date( start + selection );
-    console.log(newDate);
-    return newDate;
-  };
-
-
-  $scope.dateRange = function() {
-    dateService.dateRange();
-  }
+  $scope.dateInfo = dateService.getDateInfo();
 
 }]);
--- js/services/dateService.js
+++ js/services/dateService.js
@@ -1,36 +1,27 @@
 financialApp.factory('dateService', [function(){
 
-  var _startDate = new Date("1-2-2014");
-  var _endDate = new Date("7-31-2014");
+  var _dateInfo = { startDate: new Date("1-2-2014"),
+                    endDate: new Date("7-31-2014"),
+                    dateSelection: 0,
+                    currentDate: function() {
+                      var start = this.startDate.getTime();
+                      var selection = this.dateSelection * (1000*60*60*24);
+                      var newDate = new Date( start + selection );
+                      console.log(newDate)
+                      return newDate;
+                    },
+                    dateRange: function() {
+                      var range = Math.round( ( this.endDate.getTime() - this.startDate.getTime() ) / (1000*60*60*24) );
+                        return range;
+                    }
+                  };
 
-  var getStartDate = function(){
-    return _startDate;
-  };
-
-  var getEndDate = function(){
-    return _endDate;
-  };
-
-  var getDateSelection = function(){
-    return _dateSelection["selection"];
-  };
-
-  var convertDate = function(date){
-    return (new Date(date));
-  };
-
-  var dateRange = function(){
-    var range = Math.round( ( _endDate.getTime() -_startDate.getTime() ) / (1000*60*60*24) );
-      return range;
+  var getDateInfo = function(date) {
+    return _dateInfo;
   };
 
   return{
-    convertDate: convertDate,
-    dateRange: dateRange,
-    getStartDate: getStartDate,
-    getEndDate: getEndDate,
-    getDateSelection: getDateSelection
-
+    getDateInfo: getDateInfo,
   };
 
 }]);
