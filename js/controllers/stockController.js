6adc0a5b0f7cf6fe43ae88a56a3c278962094bde
Thomas Lo <thomasjinlo@gmail.com>
2016-03-29 12:03:35 -0700

    Fix the BUGgit add -A

--- js/controllers/stockController.js
+++ js/controllers/stockController.js
@@ -1,5 +1,8 @@
 fideligard.controller('stockCtrl',
   ['$scope', 'stockService',
    function($scope, stockService) {
+     console.log("gets here");
+     $scope.hi = "hello";
+     $scope.stockData = stockService.getStockData();
    }
 ]);
--- js/services/stockService.js
+++ js/services/stockService.js
@@ -2,24 +2,40 @@ fideligard.factory('stockService',
   ['$http',
     function($http) {
 
-      var _stocks = [];
-      var _symbols = ['GOOG', 'AMBA', 'ABM', 'AAPL', 'PSEC', 'RAVE', 'ODFL', 'YHOO', 'IEZ', 'NE'];
+      var _rawData = {};
+      var _stocks = {};
+      var _symbols = ['GOOG', 'AMBA', 'ABM', 'RAVE', 'ODFL', 'YHOO', 'IEZ', 'NE'];
 
-      var query = (function() {
-        console.log("running query");
-        $http.get('http://query.yahooapis.com/v1/public/yql?q=%20select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20=%20%22AAPL%22%20and%20startDate%20=%20%222015-01-01%22%20and%20endDate%20=%20%222015-12-31%22%20&format=json%20&diagnostics=true%20&env=store://datatables.org/alltableswithkeys%20&callback=')
-          .then(
-            function(response) {
-              console.log(response);
-            },
-            function(data) {
-              console.log("data");
-            }
-          );
-        console.log("after query");
+      var getCompany = function(symbol) {
+
+        $http.get('http://query.yahooapis.com/v1/public/yql?q=%20select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20=%20%22' + symbol + '%22%20and%20startDate%20=%20%222015-01-01%22%20and%20endDate%20=%20%222015-12-31%22%20&format=json%20&diagnostics=true%20&env=store://datatables.org/alltableswithkeys%20&callback=')
+        .then(
+          function(response){
+            _rawData[symbol] = response;
+          },
+          function(data) {
+            console.log('error' + 'company name ' + symbol);
+          }
+        );
+      };
+
+      var getAll = (function() {
+        for (var i = 0; i < _symbols.length; i++) {
+          getCompany(_symbols[i]);
+          parseData(symbol);
+        }
       })();
 
-    return {
-      
-    };
+      var parseData = function(symbol) {
+        _stocks[symbol] = _rawData[symbol].data.query.results.quote;
+      };
+
+      var getStockData = function() {
+        return _stocks;
+      };
+
+      return {
+        getStockData: getStockData
+      };
+
 }]);
