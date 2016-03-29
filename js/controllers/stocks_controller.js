a88c61def6a2a374097c5354a752eae2687b6fae
Sam O'Keefe <samok13@gmail.com>
2016-03-29 11:39:12 -0700

    working on slider

--- js/controllers/stocks_controller.js
+++ js/controllers/stocks_controller.js
@@ -2,4 +2,17 @@ financialApp.controller('StocksCtrl', ['$scope', function($scope){
 
   console.log("hello");
 
+  $scope.startDate = (new Date("1-2-2014")).getTime();
+  $scope.endDate = (new Date("7-31-2014")).getTime();
+  $scope.currentDate = (new Date("1-2-2014")).getTime();
+
+  $scope.convertDate = function(date){
+    return (new Date(date));
+  }
+
+
+var someDate = new Date();
+var numberOfDaysToAdd = 6;
+someDate.setDate(someDate.getDate() + numberOfDaysToAdd);
+
 }]);
