22b13bf59d372367ad37c0e9cf25fe024b5b4179
Julia Herron <julia.herron@gmail.com>
2016-03-29 09:58:26 -0700

    that was nuts, but it works

--- controllers/datepicker_controller.js
+++ controllers/datepicker_controller.js
@@ -5,15 +5,27 @@ fideligard.controller('DatePickerCtrl', ['$scope', function($scope) {
   // milliseconds per day: 86400000
   $scope.date = 1388563200000;
 
-  
 
-  $scope.dateString = function() {
+
+  $scope.msToString = function(ms) {
     var d = new Date();
-    d.setTime($scope.date)
-    return d.toLocaleDateString(); // '2014-03-04'
+    d.setTime(ms)
+    // $scope.date = ms;
+    // return d; // '2014-03-04'
+    $scope.dateString = d;
+  };
+
+
+  $scope.dateString = '2014-01-01';
+
+
+  $scope.stringToMs = function(string) {
+    var d = new Date(string);
+    $scope.date = d.getTime();
+    // return d.getTime();
   }
 
-  // $scope.inputDate = $filter($scope.date)(Date.now(), 'yyyy-MM-dd');
+
 
   $scope.inputDate = function() {
     var d = new Date();
