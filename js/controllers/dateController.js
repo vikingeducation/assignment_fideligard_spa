69d9b420645b233043253843faacb4f0876739f8
Andrew Baik <and.baik@gmail.com>
2016-03-29 13:21:18 -0400

    date picker works

--- js/controllers/dateController.js
+++ js/controllers/dateController.js
@@ -1,8 +1,9 @@
 fideligard.controller('dateCtrl',
   ['$scope',
    function($scope) {
-     $scope.startDateMs = (1420070400);
-     $scope.endDateMs = (1451520000);
+     $scope.startDateMs = 1420070400; // Jan 1, 2015 UTC
+     $scope.endDateMs = 1451520000; // Dec 31, 2015 UTC
+     $scope.date = $scope.startDateMs;
 
    }
 ]);
