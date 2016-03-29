ae9e05ef7d8b3bfeaf65d39fc7111a6fdf663d32
Andrew Baik <and.baik@gmail.com>
2016-03-29 17:19:46 -0400

    pseudocode for historyService

---
+++ js/services/historyService.js
@@ -0,0 +1,19 @@
+fideligard.factory('historyService',
+  ['stockService',
+    function(stockService) {
+
+      var _date = {};
+
+      {
+        "2015-01-01": {
+                          GOOG: {},
+
+                      },
+
+      }
+
+      var getPrice(date) {
+        parse date
+        return _date[]
+      }
+}]);
