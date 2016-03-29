1d2de5787036a409757e662cbd8e395985bc91d9
Andrew Baik <and.baik@gmail.com>
2016-03-29 19:01:28 -0400

    working on connecting date to stock view

--- js/controllers/dateController.js
+++ js/controllers/dateController.js
@@ -3,6 +3,16 @@ fideligard.controller('dateCtrl',
    function($scope) {
      $scope.startDateMs = 1420070400; // Jan 1, 2015 UTC
      $scope.endDateMs = 1451520000; // Dec 31, 2015 UTC
-     $scope.date = { date: $scope.startDateMs };
+     $scope.date = {
+       dateMs: $scope.startDateMs,
+       properDate: function() {
+         console.log("hello");
+         var dateObj = new Date(Date.UTC($scope.date.dateMs) * 1000);
+         var year = dateObj.getFullYear().toString();
+         var month = (dateObj.getMonth()+1).toString();
+         var day = dateObj.getDate().toString();
+         return year + "-" + month + "-" + day;
+       }
+    };
    }
 ]);
