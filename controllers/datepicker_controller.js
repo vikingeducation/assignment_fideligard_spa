769495fc7c68f960c4427bb54adb4c3305fc4709
Julia Herron <julia.herron@gmail.com>
2016-03-29 10:56:51 -0700

    form validation

--- controllers/datepicker_controller.js
+++ controllers/datepicker_controller.js
@@ -1,8 +1,9 @@
 fideligard.controller('DatePickerCtrl', ['$scope', function($scope) {
 
   // 2014-01-01: 1388563200000
-  // thru 2014-12-31: 1420012800000
-  // milliseconds per day: 86400000
+  // 2014-12-31: 1420012800000
+
+  // these are for the datepicker slider, which works in ms:
   $scope.date = 1388563200000;
 
   $scope.msToString = function(ms) {
@@ -11,12 +12,15 @@ fideligard.controller('DatePickerCtrl', ['$scope', function($scope) {
     $scope.dateString = d;
   };
 
-
+  // these are for the datepicker input box, which works in strings:
   $scope.dateString = '2014-01-01';
 
-  $scope.stringToMs = function(string) {
-    var d = new Date(string);
-    $scope.date = d.getTime();
+  $scope.stringToMs = function(string, formIsValid) {
+    console.log(formIsValid)
+    if (formIsValid) {
+      var d = new Date(string);
+      $scope.date = d.getTime();
+    }
   };
 
 
