78e9f90d1c30dcce7d3e1565191cea10f7ccdb6a
Joshua Masland-Sarani <j.masland@gmail.com>
2016-03-29 15:09:04 -0400

    debugging

--- js/controllers/stocks_controller.js
+++ js/controllers/stocks_controller.js
@@ -2,8 +2,8 @@ financialApp.controller('StocksCtrl', ['$scope', function($scope){
 
   console.log("hello");
 
-  $scope.startDate = (new Date("1-2-2014")).getTime();
-  $scope.endDate = (new Date("7-31-2014")).getTime();
+  $scope.startDate = new Date("1-2-2014");
+  $scope.endDate = new Date("7-31-2014");
   $scope.dateSelection = 0;
 
   $scope.convertDate = function(date){
@@ -11,13 +11,18 @@ financialApp.controller('StocksCtrl', ['$scope', function($scope){
   }
 
   $scope.currentDate = function() {
-    var newDate = new Date();
-    newDate.setDate($scope.startDate + $scope.dateSelection);
+    var newDate = new Date( $scope.startDate.getTime() );
+    newDate.setDate( newDate.getDate() + $scope.dateSelection );
     return newDate;
   }
 
   $scope.dateRange = function() {
-    return Math.floor(($scope.endDate - $scope.startDate)/(1000*60*60*24));
+    var newDate = Math.floor( ( $scope.endDate.getTime() - $scope.startDate.getTime() )/(1000*60*60*24));
+
+    console.log($scope.dateSelection)
+    console.log(newDate)
+
+    return newDate;
   }
 
 }]);
