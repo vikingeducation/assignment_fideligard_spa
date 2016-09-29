6c3b3949b02a7ed20e533153aacc52201a3aaf1e
Dylan Lynch <lynchd2@tcnj.edu>
2016-09-29 17:36:53 -0400

    Date conversion

--- controllers/dateController.js
+++ controllers/dateController.js
@@ -8,11 +8,14 @@ app.controller("dateCtrl", ["$scope", 'dateService', function($scope, dateServic
 
   $scope.datePicked = "2015-06-30"
   $scope.$watch("datePicked", function() {
-    var newDate = new Date(new Date(2015, 1, 0).getTime()+(parseInt($scope.datePicked)) * 24 * 60 * 60 * 1000);
-    var year =  newDate.getFullYear();
-    var month = newDate.getMonth();
-    var day = newDate.getDate();
-    $scope.newDate = year + "-" + month + "-" + day
+    //var newDate = new Date(new Date(2015, 01, 01).getTime()+(parseInt($scope.datePicked)) * 24 * 60 * 60 * 1000);
+    var d = new Date(2015,0, 0)
+    var newDate = new Date(d.setTime(d.getTime() + parseInt($scope.datePicked) * 86400000))
+    $scope.newDate = newDate
+    // var year =  newDate.getFullYear();
+    // var month = newDate.getMonth();
+    // var day = newDate.getDate();
+    // $scope.newDate = year + "-" + month + "-" + day
   })
   // $scope.convertDate = function(day) {
   //   $scope.newDate = new Date(new Date(2015, 0, 0).getTime()+(parseInt(day)) * 24 * 60 * 60 * 1000);
