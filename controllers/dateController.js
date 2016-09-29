ffe58226ce10dd9e431dff191979fece49695755
Dylan Lynch <lynchd2@tcnj.edu>
2016-09-29 17:10:40 -0400

    Date scroller

--- controllers/dateController.js
+++ controllers/dateController.js
@@ -6,4 +6,16 @@ app.controller("dateCtrl", ["$scope", 'dateService', function($scope, dateServic
     $scope.max = "";
   }
 
+  $scope.datePicked = "2015-06-30"
+  $scope.$watch("datePicked", function() {
+    var newDate = new Date(new Date(2015, 1, 0).getTime()+(parseInt($scope.datePicked)) * 24 * 60 * 60 * 1000);
+    var year =  newDate.getFullYear();
+    var month = newDate.getMonth();
+    var day = newDate.getDate();
+    $scope.newDate = year + "-" + month + "-" + day
+  })
+  // $scope.convertDate = function(day) {
+  //   $scope.newDate = new Date(new Date(2015, 0, 0).getTime()+(parseInt(day)) * 24 * 60 * 60 * 1000);
+  // }
+
 }])
