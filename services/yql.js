07b415270fa00aa077404acada0c5a8a2ebfcb51
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-09-29 16:41:20 -0700

    Parses data from yahoo

--- services/yql.js
+++ services/yql.js
@@ -2,32 +2,60 @@ Fideligard.factory('yqlService', ['$http', function($http) {
 
   var fideligardService = {};
   var _companies = ["AAPL","ABC"];
+  var _start = "2015/01/01";
+  var _end = "2015/12/31";
 
   var _quotes = [];
 
-  var organize = function(quotes) {
+  var _organize = function(quotes) {
+    var startDate = new Date();
+
     for (quote in _quotes) {
+
 
     }
   }
 
   var parseData = function(response) {
     var data = response.data.query.results.quote;
-    for (quote in data) {
-      if (_quotes[quote.symbol]) {
-        _quotes[quote.symbol]
-      }
+
+    var startDate = new Date(_start);
+    var currentDate = new Date(_start);
+    var endDate = new Date(_end);
+    endDate.setDate(endDate.getDate() + 1)
+    var quotes = [];
+
+    // add dates to an array
+    while ( currentDate < endDate ) {
+      var dateString = currentDate.toISOString().slice(0,10);
+      var dateObj = {
+        date: dateString,
+        data: {}
+      };
+      quotes.push(dateObj);
+
+      currentDate.setDate(currentDate.getDate() + 1);
+
     }
+
+    for (var index in data) {
+      var dayDiff= new Date(data[index].Date) - startDate;
+      var dateIndex = Math.ceil(dayDiff / (1000 * 3600 * 24));
+      var symbol = data[index].Symbol;
+      quotes[dateIndex].data[symbol] = data[index];
+    }
+
+    return quotes;
   };
 
-  var urlBuilder = function(companyArray, start, end) {
+  var urlBuilder = function(companyArray) {
     var companyString = "'" + companyArray.join("','") + "'";
 
     return 'http://query.yahooapis.com/v1/public/yql?q=' +
               'select * from yahoo.finance.historicaldata ' +
               'where symbol in (' + companyString + ') ' +
-              'and startDate = "' + start + '" ' +
-              'and endDate = "' + end + '" ' +
+              'and startDate = "' + _start + '" ' +
+              'and endDate = "' + _end + '" ' +
               '&format=json '+
               '&diagnostics=true ' +
               '&env=store://datatables.org/alltableswithkeys ' +
@@ -35,9 +63,10 @@ Fideligard.factory('yqlService', ['$http', function($http) {
   };
 
   fideligardService.apiStocks = function() {
-    var url = urlBuilder(_companies, "2015-01-01", "2015-12-31");
-
+    var url = urlBuilder(_companies);
+    console.log("sending data")
     return $http.get(url).then(function(response) {
+      console.log("data returned")
       angular.copy( parseData(response), _quotes );
     }) ;
   };
