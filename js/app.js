43c01cec8cf80181fd3209449097e82cf4dbf00e
David Jiang <davidjgrey@gmail>
2016-09-29 10:10:13 -0700

    routes templates and controllers setup

---
+++ js/app.js
@@ -0,0 +1,40 @@
+"use strict";
+var app = angular.module('stocks', ['ui-router']);
+
+//routes
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise('/fideligard');
+
+	$stateProvider
+		.state('main', {
+			url: '/fideligard',
+			controller: "MainCtrl",
+			views: {
+				'stocksTicker':{
+					templateUrl: 'js/templates/stocksTicker.html'
+				},
+				'dateSlider':{
+					templateUrl: 'js/templates/dateSlider.html'
+				},
+				'mainContent':{
+					templateUrl: 'js/templates/mainContent.html'
+				}
+			}
+		})
+		.state('main.portfolio', {
+			url: '',
+			templateUrl: 'js/templates/portfolio.html',
+			controller: 'PortfolioCtrl'
+		})
+		.state('main.trade', {
+			url: '/trade',
+			templateUrl: 'js/templates/trade.html',
+			controller: 'TradeCtrl'
+		})
+		.state('main.transactions', {
+			url: '/transactions',
+			templateUrl: 'js/templates/transactions.html',
+			controller: 'TransactionsCtrl'
+		});
+
+}]);
