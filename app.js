334b922b893400858721f09f9dca0a0d2e73cabe
Mike Lee <asackofwheat@gmail.com>
2016-09-29 14:13:45 -0400

    organizing the spa

---
+++ app.js
@@ -0,0 +1,47 @@
+var Fideligard = angular.module('Fideligard', ['ui.router']);
+
+Fideligard.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+
+  $urlRouterProvider.otherwise("");
+
+  $stateProvider.state('dashboard', {
+    url: '',
+    views: {
+      'range@': {
+        templateUrl: 'templates/range.html'
+      },
+
+      'stocks@': {
+        templateUrl: 'templates/stocks.html'
+      }
+    }
+  })
+
+  .state('dashboard.portfolio', {
+    url: '/portfolio',
+    views: {
+      '@': {
+        templateUrl: 'templates/portfolio.html'
+      }
+    }
+  })
+
+  .state('dashboard.trade', {
+    url: '/trade',
+    views: {
+      '@': {
+        templateUrl: 'templates/trade.html'
+      }
+    }
+  })
+
+  .state('dashboard.transaction', {
+    url: '/transaction',
+    views: {
+      '@': {
+        templateUrl: 'templates/transaction.html'
+      }
+    }
+  });
+
+}]);
