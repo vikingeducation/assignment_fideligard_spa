02702cca7d164a2af47e3dcf5609fbf069609dd0
Morgan Martin <morganm5201@gmail.com>
2016-09-29 19:59:15 -0400

    service

--- controllers/stocks_panel_controller.js
+++ controllers/stocks_panel_controller.js
@@ -1,10 +1,11 @@
 fideligardApp.controller('stocksPanelCtrl', ['$scope', 'stocksService', function($scope, stocksService){
 
   stocksService.all().then(function(stocks) {
-    $scope.allDays = stocks;
-    console.log(stocks);
+    // console.log(stocks);
+    $scope.stocks = stocksService.getStocks();
+    $scope.dates = stocksService.getDates();
+    console.log($scope.stocks);
+    console.log($scope.dates);
   });
-
-  $scope.stocks = stockService.all();
 
 }]);
--- services/stocks_service.js
+++ services/stocks_service.js
@@ -2,8 +2,8 @@ fideligardApp.factory('stocksService', ['$http', "$q", function($http, $q){
 
   var stocksService = {};
   var _selectedDate = {};
-  var _stocks = [];
-  var _symbols = ["MGT", "EBIO", "SPHS", "X", "SRPT", "WLL", "REN", "VRX", "CBS", "RH", "CLF", "CHK"]
+  var _stocks = {};
+  var _symbols = ["MGT", "EBIO", "SPHS", "X", "SRPT", "WLL", "REN", "VRX", "CBS", "RH", "CLF", "CHK"];
 
   stocksService.getSelectedDate = function() {
     return _selectedDate;
@@ -13,27 +13,45 @@ fideligardApp.factory('stocksService', ['$http', "$q", function($http, $q){
     angular.copy(_selectedDate, date);
   };
 
-
-  var _stocks = [];
+  var _addEntriesToStocks = function(arr){
+    var stock;
+    for(var i = 0; i < arr.length; i++){
+      stock = arr[i];
+      console.log(stock.Date);
+      if(_stocks[stock.Date]) {
+        _stocks[stock.Date].push(stock);
+      } else {
+        _stocks[stock.Date] = [stock];
+      }
+    }
+  };
 
   stocksService.all = function() {
-
     var requests = [];
     for (var i = 0; i < _symbols.length; i++) {
       requests.push($http({
         url: "http://query.yahooapis.com/v1/public/yql?q=%20select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20=%20%22" + _symbols[i] + "%22%20and%20startDate%20=%20%222014-01-01%22%20and%20endDate%20=%20%222014-12-31%22%20&format=json%20&diagnostics=true%20&env=store://datatables.org/alltableswithkeys%20&callback="
       }));
-    };
+    }
 
     return $q.all(requests).then(function(response) {
+      console.log('RESPONSE');
+      console.log(response);
       for (var i = 0; i < response.length; i++) {
-        _stocks.push(response[i].data.query.results.quote);
+        _addEntriesToStocks(response[i].data.query.results.quote);
       }
-      console.log(_stocks);
       return _stocks;
     }, function(response) {
       console.error(response);
     });
+  };
+
+  stocksService.getStocks = function() {
+    return _stocks;
+  };
+
+  stocksService.getDates = function() {
+    return Object.keys(_stocks).sort();
   };
 
 
