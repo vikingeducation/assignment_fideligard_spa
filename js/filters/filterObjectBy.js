64b0243cb15c1d840b6e37d1c45ba795f8cd9c98
cjvirtucio87 <cjv287@gmail.com>
2016-09-30 16:11:07 -0700

    Changed data

--- js/filters/filterObjectBy.js
+++ js/filters/filterObjectBy.js
@@ -1,6 +1,7 @@
 app.filter('filterObjectBy', ['_', function(_) {
-  return function(items, value, symbol) {
+  return function(items, value) {
     return _.filter(items,function(item) {
+      var symbol = Object.keys(item)[0];
       return item[symbol]['Date'] === value;
     });
   };
--- js/services/stock_service.js
+++ js/services/stock_service.js
@@ -4,8 +4,18 @@ app.factory('StockService', ['$http', function($http) {
   var _symbols = [];
   var _dates = [];
 
+  var YQL_QUERY = "http://query.yahooapis.com/v1/public/yql?q=\
+        select * from   yahoo.finance.historicaldata\
+                 where  symbol    in (\"AAPL\", \"GOOGL\", \"TWTR\")\
+                 and    startDate = \"2011-09-11\"\
+                 and    endDate   = \"2014-02-11\"\
+        &format=json\
+        &diagnostics=true\
+        &env=store://datatables.org/alltableswithkeys\
+        &callback=";
+
   StockService.queryStocks = function() {
-    return $http.get("http://query.yahooapis.com/v1/public/yql?q=%20select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20=%20%22AAPL%22%20and%20startDate%20=%20%222014-01-01%22%20and%20endDate%20=%20%222014-12-31%22%20&format=json%20&diagnostics=true%20&env=store://datatables.org/alltableswithkeys%20&callback=")
+    return $http.get(YQL_QUERY)
             .then(function(response) {
               // Grab dates.
               _.forEach(
@@ -103,22 +113,6 @@ app.factory('StockService', ['$http', function($http) {
   StockService.getDates = function() {
     return _dates;
   };
-
-
-  // Symbol: {
-  //   Year: {
-  //     Days: [
-  //     ]
-  //   }
-  //   Year: {
-  //     Days: [
-  //     ]
-  //   }
-  //   Year: {
-  //     Days: [
-  //     ]
-  //   }
-  // }
 
   return StockService;
 
