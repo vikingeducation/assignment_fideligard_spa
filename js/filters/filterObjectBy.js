8c69ffa5a9759794b0f174438f30c3492de33f8e
cjvirtucio87 <cjv287@gmail.com>
2016-10-01 10:49:48 -0700

    Date filter works, but broke day changes

--- js/filters/filterObjectBy.js
+++ js/filters/filterObjectBy.js
@@ -1,6 +1,7 @@
 app.filter('filterObjectBy', ['_', function(_) {
   return function(items, value) {
     return _.filter(items,function(item) {
+      console.log(value);
       var symbol = Object.keys(item)[0];
       return item[symbol]['Date'] === value;
     });
