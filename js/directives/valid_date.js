8c994e20c19bf1ff7488334b47b96279214c65e7
cjvirtucio87 <cjv287@gmail.com>
2016-10-02 09:51:42 -0700

    latestStock works, but need to differentiate between symbols.

--- js/directives/valid_date.js
+++ js/directives/valid_date.js
@@ -1,5 +1,6 @@
 app.directive('validDate',
-['StockService', 'DateService', function(StockService, DateService) {
+['StockService', 'DateService', 'TradeService',
+function(StockService, DateService, TradeService) {
 
   return {
     restrict: 'A',
@@ -7,10 +8,25 @@ app.directive('validDate',
     link: function(scope, elem, attrs, ctrl) {
       ctrl.$validators.date = function(modelValue, viewValue) {
         var modelDate = modelValue;
+
         if (modelDate) {
           modelDate = DateService.stringifyDate(modelValue);
         }
-        return _.includes(StockService.getDates(), modelDate);
+
+        console.log('Model Value');
+        console.log(modelValue);
+
+        var inRange = _.includes(StockService.getDates(), modelDate);
+        // Time travel problem. Shouldn't be able to buy stock at date,
+        // then buy stock at earlier date for that symbol.
+        var latestStock = TradeService.getLatestStock(scope.trade.formData.symbol);
+
+        console.log('Latest Stock');
+        console.log(latestStock);
+
+        var afterLatest = modelValue >= latestStock;
+
+        return latestStock ? (inRange && afterLatest) : inRange;
       };
     }
   };
--- js/services/trade_service.js
+++ js/services/trade_service.js
@@ -1,52 +1,60 @@
 app.factory('TradeService', [function() {
   var TradeService = {};
   var _tradeInfo = {
-    trade: {},
+    tradeData: {},
     user: {
       id: 1,
       username: '',
       owned: {},
       cashAvailable: 10000,
-    }
+    },
   };
 
   TradeService.placeOrder = function (trade) {
-    _tradeInfo.trade = trade;
-
-    var symbol = trade.formData.symbol;
-    var owned = _tradeInfo.user.owned;
+    _tradeInfo.tradeData = trade.formData;
+    _tradeInfo.user = trade.user;
 
     // Increment/set quantity of owned stocks for that symbol.
-    switch (trade.type) {
+    switch (trade.formData.type) {
       case 'buy':
-        if (trade.formData.cost > _tradeInfo.user.cashAvailable) {
+        if (_tradeInfo.tradeData.cost > _tradeInfo.user.cashAvailable) {
           throw new Error("Invalid purchase! Can't buy more than you can afford!");
         }
-        if (owned[symbol]) {
-          owned[symbol].quantity += trade.formData.quantity;
+        if (_tradeInfo.user.owned[_tradeInfo.tradeData.symbol]) {
+          _tradeInfo.user.owned[_tradeInfo.tradeData.symbol].quantity += _tradeInfo.tradeData.quantity;
+          _tradeInfo.user.owned[_tradeInfo.tradeData.symbol].latestStock = _tradeInfo.tradeData.date;
         } else {
-          _tradeInfo.user.owned[symbol] = {
-            quantity: trade.formData.quantity
+          _tradeInfo.user.owned[_tradeInfo.tradeData.symbol] = {
+            quantity: _tradeInfo.tradeData.quantity,
+            latestStock: _tradeInfo.tradeData.date
           };
+          console.log(_tradeInfo.user.owned);
         }
         break;
       case 'sell':
-        if (owned[symbol]) {
-          owned[symbol].quantity -= trade.formData.quantity;
-          if (owned[symbol].quantity - trade.formData.quantity <= 0) {
-            delete owned[symbol];
+        if (tradeInfo.user.owned[_tradeInfo.tradeData.symbol]) {
+          tradeInfo.user.owned[_tradeInfo.tradeData.symbol].quantity -= _tradeInfo.tradeData.quantity;
+          if (tradeInfo.user.owned[_tradeInfo.tradeData.symbol].quantity - _tradeInfo.tradeData.quantity <= 0) {
+            delete tradeInfo.user.owned[_tradeInfo.tradeData.symbol];
           }
         } else {
           throw new Error("Invalid sale! Cannot sell what you don't own.");
         }
+        break;
     }
 
 
-    _tradeInfo.user.cashAvailable -= trade.formData.cost;
+    _tradeInfo.user.cashAvailable -= _tradeInfo.tradeData.cost;
   };
 
   TradeService.setUserData = function (trade) {
     trade.user = _tradeInfo.user;
+  };
+
+  TradeService.getLatestStock = function (symbol) {
+    if (_tradeInfo.user.owned[symbol]) {
+      return _tradeInfo.user.owned[symbol].latestStock;
+    }
   };
 
   return TradeService;
