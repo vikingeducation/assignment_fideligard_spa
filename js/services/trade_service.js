505fe360586c4b387e6e33c1bb82807e04eeea59
cjvirtucio87 <cjv287@gmail.com>
2016-10-02 11:42:07 -0700

    Refactored trade service.

--- js/services/trade_service.js
+++ js/services/trade_service.js
@@ -10,63 +10,67 @@ app.factory('TradeService', [function() {
     },
   };
 
+  // Helpers for _adjustQuant.
+  function _buy (tradeInfo, date, symbol, tradeQuant) {
+    // If he owns stock for that day.
+    if (tradeInfo.user.owned[date]) {
+      // If he owns stock for that symbol.
+      if (tradeInfo.user.owned[symbol]) {
+        tradeInfo.user.owned[date][symbol].quantity += tradeQuant;
+      } else {
+        tradeInfo.user.owned[date][symbol] = {
+          quantity: tradeQuant
+        };
+      }
+    // Otherwise, initialize the objects.
+    } else {
+      tradeInfo.user.owned[date] = {};
+      tradeInfo.user.owned[date][symbol] = {
+        quantity: tradeQuant
+      };
+    }
+  }
+
+  function _sell (tradeInfo) {
+    // If he owns stock for that day
+    if (tradeInfo.user.owned[date]) {
+      // If he owns stock for that symbol
+      if (tradeInfo.user.owned[date][symbol]) {
+        tradeInfo.user.owned[date][symbol].quantity -= tradeQuant;
+        if (tradeInfo.user.owned[date][symbol].quantity <= 0) {
+          delete tradeInfo.user.owned[date][symbol];
+        }
+        if (_.isEmpty(tradeInfo.user.owned[date])) {
+          delete tradeInfo.user.owned[date];
+        }
+      }
+    }
+  }
+
+  // Increment/set quantity of owned stocks for that symbol.
+  function _adjustQuant (tradeInfo, trade) {
+    switch (trade.formData.type) {
+      case 'buy':
+        if (tradeInfo.tradeData.cost > tradeInfo.user.cashAvailable) {
+          throw new Error("Invalid purchase! Can't buy more than you can afford!");
+        }
+
+        var date = tradeInfo.tradeData.date;
+        var symbol = tradeInfo.tradeData.symbol;
+        var tradeQuant = tradeInfo.tradeData.quantity;
+
+        _buy(tradeInfo, date, symbol, tradeQuant);
+        break;
+      case 'sell':
+        _sell(tradeInfo);
+        break;
+    }
+  }
+
   TradeService.placeOrder = function (trade) {
     _tradeInfo.tradeData = trade.formData;
     _tradeInfo.user = trade.user;
-
-    // Increment/set quantity of owned stocks for that symbol.
-    switch (trade.formData.type) {
-      case 'buy':
-        if (_tradeInfo.tradeData.cost > _tradeInfo.user.cashAvailable) {
-          throw new Error("Invalid purchase! Can't buy more than you can afford!");
-        }
-        // If he owns stock for that day.
-        if (_tradeInfo.user.owned[_tradeInfo.tradeData.date]) {
-          // If he owns stock for that symbol.
-          if (_tradeInfo.user.owned[_tradeInfo.tradeData.symbol]) {
-            _tradeInfo.user.owned[_tradeInfo.tradeData.date][_tradeInfo.tradeData.symbol].quantity += _tradeInfo.tradeData.quantity;
-          } else {
-            _tradeInfo.user.owned[_tradeInfo.tradeData.date][_tradeInfo.tradeData.symbol] = {
-              quantity: _tradeInfo.tradeData.quantity
-            };
-          }
-        // Otherwise, initialize the objects.
-        } else {
-          _tradeInfo.user.owned[_tradeInfo.tradeData.date] = {};
-          _tradeInfo.user.owned[_tradeInfo.tradeData.date][_tradeInfo.tradeData.symbol] = {
-            quantity: _tradeInfo.tradeData.quantity
-          };
-        }
-
-        // if (_tradeInfo.user.owned[_tradeInfo.tradeData.symbol]) {
-        //   _tradeInfo.user.owned[_tradeInfo.tradeData.symbol].quantity += _tradeInfo.tradeData.quantity;
-        //   _tradeInfo.user.owned[_tradeInfo.tradeData.symbol].latestStock = _tradeInfo.tradeData.date;
-        // } else {
-        //   _tradeInfo.user.owned[_tradeInfo.tradeData.symbol] = {
-        //     quantity: _tradeInfo.tradeData.quantity,
-        //     latestStock: _tradeInfo.tradeData.date
-        //   };
-        //   console.log(_tradeInfo.user.owned);
-        // }
-        break;
-      case 'sell':
-        // If he owns stock for that day
-        if (_tradeInfo.user.owned[_tradeInfo.tradeData.date]) {
-          // If he owns stock for that symbol
-          if (_tradeInfo.user.owned[_tradeInfo.tradeData.date][_tradeInfo.tradeData.symbol]) {
-            _tradeInfo.user.owned[_tradeInfo.tradeData.date][_tradeInfo.tradeData.symbol].quantity -= _tradeInfo.tradeData.quantity;
-            if (_tradeInfo.user.owned[_tradeInfo.tradeData.date][_tradeInfo.tradeData.symbol].quantity <= 0) {
-              delete _tradeInfo.user.owned[_tradeInfo.tradeData.date][_tradeInfo.tradeData.symbol];
-            }
-            if (_.isEmpty(_tradeInfo.user.owned[_tradeInfo.tradeData.date])) {
-              delete _tradeInfo.user.owned[_tradeInfo.tradeData.date];
-            }
-          }
-        }
-        break;
-    }
-
-
+    _adjustQuant(_tradeInfo, trade);
     _tradeInfo.user.cashAvailable -= _tradeInfo.tradeData.cost;
   };
 
