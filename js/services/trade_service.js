3c036e2e02fe58982e2f57e367c00b93cae01eca
cjvirtucio87 <cjv287@gmail.com>
2016-10-02 07:06:17 -0700

    Added db-level validations.

--- js/services/trade_service.js
+++ js/services/trade_service.js
@@ -13,14 +13,34 @@ app.factory('TradeService', [function() {
   TradeService.placeOrder = function (trade) {
     _tradeInfo.trade = trade;
 
+    var symbol = trade.formData.symbol;
+    var owned = _tradeInfo.user.owned;
+
     // Increment/set quantity of owned stocks for that symbol.
-    if (_tradeInfo.user.owned[trade.formData.symbol]) {
-      _tradeInfo.user.owned[trade.formData.symbol].quantity += trade.formData.quantity;
-    } else {
-      _tradeInfo.user.owned[trade.formData.symbol] = {
-        quantity: trade.formData.quantity
-      };
+    switch (trade.type) {
+      case 'buy':
+        if (trade.formData.cost > _tradeInfo.user.cashAvailable) {
+          throw new Error("Invalid purchase! Can't buy more than you can afford!");
+        }
+        if (owned[symbol]) {
+          owned[symbol].quantity += trade.formData.quantity;
+        } else {
+          _tradeInfo.user.owned[symbol] = {
+            quantity: trade.formData.quantity
+          };
+        }
+        break;
+      case 'sell':
+        if (owned[symbol]) {
+          owned[symbol].quantity -= trade.formData.quantity;
+          if (owned[symbol].quantity - trade.formData.quantity <= 0) {
+            delete owned[symbol];
+          }
+        } else {
+          throw new Error("Invalid sale! Cannot sell what you don't own.");
+        }
     }
+
 
     _tradeInfo.user.cashAvailable -= trade.formData.cost;
   };
