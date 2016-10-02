d8a37f303efdbf5cf96b6290d24cb71c906979ef
cjvirtucio87 <cjv287@gmail.com>
2016-10-01 18:17:14 -0700

    Added cost

--- js/controllers/stocks_trade_controller.js
+++ js/controllers/stocks_trade_controller.js
@@ -3,6 +3,11 @@ app.controller('StocksTradeCtrl',
 function($scope, StockService, TradeService) {
 
   $scope.trade = StockService.getSelectedStock();
+  $scope.trade.formData = {};
+  $scope.trade.user = {
+    cashAvailable: 1000
+  };
+
   $scope.placeOrder = function() {
     TradeService.placeOrder($scope.trade);
   };
@@ -11,10 +16,22 @@ function($scope, StockService, TradeService) {
     return $scope.trade.stock.Date ? $scope.trade.stock.Date : '2015-01-01';
   };
 
+  $scope.showCost = function() {
+    if ($scope.trade.formData.cost) {
+      return $scope.trade.formData.cost;
+    }
+  };
+
   // You have to watch what exactly is being changed. Trade object itself never
   // gets changed, so you have to watch its stock prop.
-  $scope.$watch('trade.stock', function() {
-
-  });
+  $scope.$watchGroup(
+    ['trade.formData.quantity',
+    'trade.formData.price',
+    'trade.stock.Close'],
+    function(newValues) {
+      $scope.trade.formData.price = newValues[2];
+      $scope.trade.formData.cost = parseInt(_.multiply(newValues.slice(0,1)));
+    }
+  );
 
 }]);
