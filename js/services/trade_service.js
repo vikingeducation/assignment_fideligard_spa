878ad570861a00545d7163b69ab7d8605ecae1e5
cjvirtucio87 <cjv287@gmail.com>
2016-10-02 11:30:25 -0700

    Revised owned obj.

--- js/services/trade_service.js
+++ js/services/trade_service.js
@@ -20,25 +20,48 @@ app.factory('TradeService', [function() {
         if (_tradeInfo.tradeData.cost > _tradeInfo.user.cashAvailable) {
           throw new Error("Invalid purchase! Can't buy more than you can afford!");
         }
-        if (_tradeInfo.user.owned[_tradeInfo.tradeData.symbol]) {
-          _tradeInfo.user.owned[_tradeInfo.tradeData.symbol].quantity += _tradeInfo.tradeData.quantity;
-          _tradeInfo.user.owned[_tradeInfo.tradeData.symbol].latestStock = _tradeInfo.tradeData.date;
+        // If he owns stock for that day.
+        if (_tradeInfo.user.owned[_tradeInfo.tradeData.date]) {
+          // If he owns stock for that symbol.
+          if (_tradeInfo.user.owned[_tradeInfo.tradeData.symbol]) {
+            _tradeInfo.user.owned[_tradeInfo.tradeData.date][_tradeInfo.tradeData.symbol].quantity += _tradeInfo.tradeData.quantity;
+          } else {
+            _tradeInfo.user.owned[_tradeInfo.tradeData.date][_tradeInfo.tradeData.symbol] = {
+              quantity: _tradeInfo.tradeData.quantity
+            };
+          }
+        // Otherwise, initialize the objects.
         } else {
-          _tradeInfo.user.owned[_tradeInfo.tradeData.symbol] = {
-            quantity: _tradeInfo.tradeData.quantity,
-            latestStock: _tradeInfo.tradeData.date
+          _tradeInfo.user.owned[_tradeInfo.tradeData.date] = {};
+          _tradeInfo.user.owned[_tradeInfo.tradeData.date][_tradeInfo.tradeData.symbol] = {
+            quantity: _tradeInfo.tradeData.quantity
           };
-          console.log(_tradeInfo.user.owned);
         }
+
+        // if (_tradeInfo.user.owned[_tradeInfo.tradeData.symbol]) {
+        //   _tradeInfo.user.owned[_tradeInfo.tradeData.symbol].quantity += _tradeInfo.tradeData.quantity;
+        //   _tradeInfo.user.owned[_tradeInfo.tradeData.symbol].latestStock = _tradeInfo.tradeData.date;
+        // } else {
+        //   _tradeInfo.user.owned[_tradeInfo.tradeData.symbol] = {
+        //     quantity: _tradeInfo.tradeData.quantity,
+        //     latestStock: _tradeInfo.tradeData.date
+        //   };
+        //   console.log(_tradeInfo.user.owned);
+        // }
         break;
       case 'sell':
-        if (tradeInfo.user.owned[_tradeInfo.tradeData.symbol]) {
-          tradeInfo.user.owned[_tradeInfo.tradeData.symbol].quantity -= _tradeInfo.tradeData.quantity;
-          if (tradeInfo.user.owned[_tradeInfo.tradeData.symbol].quantity - _tradeInfo.tradeData.quantity <= 0) {
-            delete tradeInfo.user.owned[_tradeInfo.tradeData.symbol];
+        // If he owns stock for that day
+        if (_tradeInfo.user.owned[_tradeInfo.tradeData.date]) {
+          // If he owns stock for that symbol
+          if (_tradeInfo.user.owned[_tradeInfo.tradeData.date][_tradeInfo.tradeData.symbol]) {
+            _tradeInfo.user.owned[_tradeInfo.tradeData.date][_tradeInfo.tradeData.symbol].quantity -= _tradeInfo.tradeData.quantity;
+            if (_tradeInfo.user.owned[_tradeInfo.tradeData.date][_tradeInfo.tradeData.symbol].quantity <= 0) {
+              delete _tradeInfo.user.owned[_tradeInfo.tradeData.date][_tradeInfo.tradeData.symbol];
+            }
+            if (_.isEmpty(_tradeInfo.user.owned[_tradeInfo.tradeData.date])) {
+              delete _tradeInfo.user.owned[_tradeInfo.tradeData.date];
+            }
           }
-        } else {
-          throw new Error("Invalid sale! Cannot sell what you don't own.");
         }
         break;
     }
@@ -52,8 +75,10 @@ app.factory('TradeService', [function() {
   };
 
   TradeService.getLatestStock = function (symbol) {
-    if (_tradeInfo.user.owned[symbol]) {
-      return _tradeInfo.user.owned[symbol].latestStock;
+    // owned obj organized by date, then symbol.
+    var latestDate = _.max(Object.keys(_tradeInfo.user.owned));
+    if (latestDate && _tradeInfo.user.owned[latestDate][symbol]) {
+      return latestDate;
     }
   };
 
