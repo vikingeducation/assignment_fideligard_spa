1989723e74c24007d9870728b0f3a600af2ef832
schoetlr <srlrs20020@gmail.com>
2016-10-03 08:32:57 -0400

    route skeleton built with templates properly rendering

--- js/app.js
+++ js/app.js
@@ -3,7 +3,43 @@ var StockApp = angular.module("StockApp", ['ui.router']);
 
 
 
+StockApp.config(function($stateProvider, $urlRouterProvider){
 
+  $urlRouterProvider.otherwise("/");
+
+
+  $stateProvider
+    .state('main', {
+      url: "",
+      views: {
+        "@": {
+          templateUrl: "/js/templates/portfolio.html",
+          controller: "PortfolioCtrl"
+        },
+        'stocks@': {
+          templateUrl: "/js/templates/stocks.html",
+          controller: "StocksCtrl"
+        },
+
+        'datepicker@': {
+          templateUrl: "/js/templates/datepicker.html",
+          controller: "DatePickerCtrl"
+        }
+      }
+    })
+
+    .state("main.stocks", {
+      url: "/stocks/:id",
+      views: {
+        "@": {
+          templateUrl: "js/templates/stock.html"
+        }
+      }
+    })
+    
+    
+
+});
 
 
 
