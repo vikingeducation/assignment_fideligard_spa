72481f0af829ac05c70b32047da8eca6f3876499
cjvirtucio87 <cjv287@gmail.com>
2016-10-03 16:51:21 -0700

    More styling

--- js/directives/route_dropdown.js
+++ js/directives/route_dropdown.js
@@ -3,6 +3,11 @@ app.directive('routeDropdown', [function () {
   return {
     restrict: 'E',
     templateUrl: 'js/directives/route_dropdown.html',
+    scope: {
+      currentView: '='
+    },
+    link: function(scope) {
+    }
   };
 
 }]);
