1db66e23af688e4136ca2087947457044fb21eed
schoetlr <srlrs20020@gmail.com>
2016-10-04 20:48:15 -0400

    updated portfolio service and template

--- js/services/portfolio_service.js
+++ js/services/portfolio_service.js
@@ -1,16 +1,26 @@
-StockApp.factory("portfolioService", [function(){
-  
+StockApp.factory("portfolioService", ['transactionService', function(transactionService){
+  //ADDING THE portfolioGenerator dependency throws nasty error
   var service = {};
 
   //Symbol is key and array of transactions is value
   _cash = 1000000;
 
+  var _portfolio;
 
+  var _buildPortfolio = function(date){
+    var transactions = transactionService.transactionsBeforeDate(date);
+    _portfolio = portfolioGenerator.generatePortfolio(transactions);
+    return _portfolio;
+  }
 
+  service.buildPortfolio = function(date){
+    _buildPortfolio(date);
+  };
+   
 
 
   service.availableCash = function(){
-    return _cash
+    return _cash;
   };
 
   return service;
