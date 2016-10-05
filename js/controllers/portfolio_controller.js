1ddf0408fa3abaa49d9c4260d85e529ee7f133af
schoetlr <srlrs20020@gmail.com>
2016-10-04 20:59:45 -0400

    working with portfolio controller updated and included

--- js/controllers/portfolio_controller.js
+++ js/controllers/portfolio_controller.js
@@ -1,8 +1,23 @@
-StockApp.controller("TransactionsCtrl", ['$scope', 'transactionService', function($scope, transactionService){
+StockApp.controller("PortfolioCtrl", ['$scope', 'portfolioService', 'transactionService', 'dateService', function($scope, portfolioService, transactionService, dateService){
   
+  
+  //sync date
   console.log("loading");
+  $scope.selectedDate = dateService.selectedDate;
+
+  $scope.$watch(function(){
+    return dateService.selectedDate;
+  }, function(newDate){
+    $scope.selectedDate = newDate;
+
+    $scope.portfolio = portfolioService.buildPortfolio(newDate);
+  });
+
+
+  //get portfolio before date
+
+
 }]);
 
 
 
-//TRANSACTION ROUTE IS NOT LOADING
