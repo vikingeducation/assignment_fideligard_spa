db9a26e8eaef32295b8676a748cecd965b78a2db
schoetlr <srlrs20020@gmail.com>
2016-10-04 20:50:16 -0400

    updated transaction service

--- js/services/transaction_service.js
+++ js/services/transaction_service.js
@@ -14,6 +14,22 @@ StockApp.factory("transactionService", [function(){
   };
 
 
+  StockApp.factory("transactionService", [function(){
+
+  var service = {};
+
+  //ticker as key and transactions as value
+  var _transactions = {};
+
+  service.transaction = function(date, symbol, type, quantity, price){
+    this.date = date;
+    this.symbol = symbol;
+    this.type = type;
+    this.quantity = quantity;
+    this.price = price;
+  };
+
+
   service.createTransaction = function(date, symbol, type, quantity, price){
     var transaction = new service.transaction(date, symbol, type, quantity, price);
     if(_transactions[symbol]){
@@ -39,10 +55,41 @@ StockApp.factory("transactionService", [function(){
     return result;
   };
 
+  var _seedTransactions = function(){
+    //var dates = ['2015-01-03','2015-01-04','2015-01-05','2015-01-06',];
+    var tickers = ['AAPL', 'NFLIX', 'CSCO', 'INTU'];
+
+    //price is just a random number for some basic seed date
+    for(var i = 0; i < 4; i++){
+      service.createTransaction('2015-01-02', tickers[i], "BUY", 50, 137)
+    }
+  };
 
 
+  _seedTransactions();
+
+
+
+  service.transactionsBeforeDate = function(date){
+    var originalDate = new Date(date);
+    var transactionsBeforeDate = {};
+
+    for(ticker in _transactions){
+      _transactions[ticker].forEach(function(transaction){
+        var tDate = new Date(transaction.date);
+        if(tDate <= originalDate){
+          if(transactionsBeforeDate[ticker]){
+            transactionsBeforeDate[ticker].push(transaction);
+          } else {
+            transactionsBeforeDate[ticker] = [];
+            transactionsBeforeDate[ticker].push(transaction);
+          }
+        }
+      });
+    };
+  };
 
 
 
   return service;
-}])
+}]);
