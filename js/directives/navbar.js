0763c88d4694122640532d3910696385a6899e1d
schoetlr <srlrs20020@gmail.com>
2016-10-06 07:18:45 -0400

    style tables

--- js/directives/navbar.js
+++ js/directives/navbar.js
@@ -3,9 +3,6 @@ StockApp.directive("navBar", function(){
   return {
     templateUrl: "js/templates/navbar.html",
     restrict: "E",
-    scope: {},
-    link: function(){
-      console.log("DIRECTIVE LOADED");
-    }
+    scope: {}
   }
 })
