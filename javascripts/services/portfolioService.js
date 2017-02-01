9200fd9ec8246e8e3939ecad3e91dee55aa40fe5
James Harris <computersherpa@gmail.com>
2017-01-31 18:43:46 -0600

    comments

--- javascripts/services/portfolioService.js
+++ javascripts/services/portfolioService.js
@@ -16,19 +16,6 @@ fideligard.factory('portfolioService', ['stockService', function(stockService) {
     byDate: {},
   };
 
-  // (function initializeDates() {
-  //   var START_DATE = new Date("Jan 01 2016");
-  //   var END_DATE = new Date("Dec 31 2016");
-  //   var current = new Date(START_DATE);
-  //   var currentDateString;
-  //   while (current < END_DATE) {
-  //     currentDateString = current.toDateString();
-  //     exports.resources[currentDateString] = {};
-  //     exports.trades.byDate[currentDateString] = {};
-  //     current = new Date(current.setDate(current.getDate() + 1));
-  //   }
-  // })();
-
   var walkDates = function(startDateString, endDateString, callback) {
     var startDate = new Date(startDateString);
     var endDate = new Date(endDateString);
@@ -50,7 +37,6 @@ fideligard.factory('portfolioService', ['stockService', function(stockService) {
   });
 
   exports.newTrade = function(symbol, quantity, dateString, buyOrSell) {
-    // console.log(symbol, quantity, dateString, buyOrSell);
     var id = nextId();
     var price = stockService.dates[dateString][symbol].closing;
     var cost = price * quantity;
@@ -71,20 +57,15 @@ fideligard.factory('portfolioService', ['stockService', function(stockService) {
     exports.trades.byDate[dateString] = exports.trades.byDate[dateString] || [];
     exports.trades.byDate[dateString].push(id);
 
+    // apply cash and stock deltas to the future
     walkDates(dateString, "Dec 31 2016", function(dateString) {
       exports.resources[dateString].cash += trade.cash;
       exports.resources[dateString][symbol] = exports.resources[dateString][symbol] || 0;
       exports.resources[dateString][symbol] += trade.quantity;
     });
 
-    console.log(exports.trades);
-    console.log(exports.resources);
-
     return trade;
   };
-
-  // var jan2String = new Date('1-2-2016').toDateString();
-  // exports.newTrade('AAPL', 50, jan2String, 'buy');
 
   return exports;
 }]);
